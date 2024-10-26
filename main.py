250eb917639e1833cf7e6b7b98856bd2fc25a553
Aleksandra <ajieksandra.93@mail.ru>
2024-10-26 14:38:06 +0500

    Добавлена обработка исключений

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from opencage.geocoder import OpenCageGeocode
 from tkinter import ttk
 from tkinter import *
+from tkinter import messagebox as mb
 
 
 def get_coordinates(city, key):
-    geocoder = OpenCageGeocode(key)
-    query = city
-    results = geocoder.geocode(query)
-    if results:
-        return results[0]['geometry']['lat'], results[0]['geometry']['lng']
-    else:
-        return 'Город не найден'
+    try:
+        geocoder = OpenCageGeocode(key)
+        query = city
+        results = geocoder.geocode(query)
+        if results:
+            return results[0]['geometry']['lat'], results[0]['geometry']['lng']
+        else:
+            return 'Город не найден'
+    except Exception as e:
+        mb.showerror('Ошибка', f'Произошла ошибка {e}')
 
 key = '34eb7c00e36444bea8ed85dfe746e82e'
-city = 'Moscow'
+city = 'London'
 coordinates = get_coordinates(city, key)
 print(f'Координаты города {city}: {coordinates}')
 
