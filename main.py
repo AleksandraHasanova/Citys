fee07b97102abcc50a1a82564805b0d18fc92bdf
Aleksandra <ajieksandra.93@mail.ru>
2024-10-26 16:50:29 +0500

    Добавлена проверка существования региона. Если региона нет, информация о нем не выводится

--- main.py
+++ main.py
@@ -11,23 +11,27 @@ def get_coordinates(city, key):
             lat = round(results[0]['geometry']['lat'], 2)
             lng = round(results[0]['geometry']['lng'], 2)
             country = results[0]['components']['country']
-            region = results[0]['components']['state']
-            label.config(text=f"Координаты города {city}:\n Широта: {lat}, Долгота: {lng},\n Страна: {country}, Регион: {region}")
+            if 'state' in results[0]['components']:
+                region = results[0]['components']['state']
+                return f"Широта: {lat}, Долгота: {lng}\n Страна: {country}. Регион: {region}"
+            else:
+                return f"Широта: {lat}, Долгота: {lng}\n Страна: {country}."
         else:
-            label.config(text=f'Город {city} не найден')
+            return f'Город {city} не найден'
     except Exception as e:
         mb.showerror('Ошибка', f'Произошла ошибка {e}')
 
 def show_coordinates(event=None):
     city = entry.get()
-    coordinates = get_coordinates(city, key)
+    result = get_coordinates(city, key)
+    label.config(text=f"Координаты города {city}:\n {result}")
 
 
 key = '34eb7c00e36444bea8ed85dfe746e82e'
 
 window = Tk()
 window.title('Информация о городах')
-window.geometry('450x150')
+window.geometry('450x170')
 
 entry = Entry()
 entry.pack(pady=10)
